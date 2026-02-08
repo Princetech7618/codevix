948defeb933051c29178529d449880dea8d130a8
9gouravpal <gouravp386@gmail.com>
2026-02-08 15:25:24 +0530

    Fixed sitemap: removed index sitemap

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -2,6 +2,7 @@
 module.exports = {
   siteUrl: 'https://softnexis.in',
   generateRobotsTxt: true,
+  generateIndexSitemap: false, // YE LINE ADD KAREIN (Zaroori hai)
   sitemapSize: 5000,
   changefreq: 'weekly',
   priority: 0.7,
